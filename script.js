ebbf09e486f408f3d54fbe0fbb1328fbadef92a2
warlyware <danward@gmail.com>
2015-06-22 16:57:28 -0700

    Add main functionality

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
 var app = angular.module('N2WApp', []);
 
 function N2WCtrl($scope) {
-  $scope.letterSets = {
-    1: [],
-    2: ['A','B','C'],
-    3: ['D','E','F'],
-    4: ['G','H','I'],
-    5: ['J','K','L'],
-    6: ['M','N','O'],
-    7: ['P','Q','R','S'],
-    8: ['T','U','V'],
-    9: ['W','X','Y','Z']
+
+  var PERMS = {
+    allCases: {
+      "1": ["1"],
+      "2": ["a", "b", "c", "2"],
+      "3": ["d", "e", "f", "3"],
+      "4": ["g", "h", "i", "4"],
+      "5": ["j", "k", "l", "5"],
+      "6": ["m", "n", "o", "6"],
+      "7": ["p", "q", "r", "s", "7"],
+      "8": ["t", "u", "v", "8"],
+      "9": ["w", "x", "y", "z", "9"],
+      "0": ["0"]
+    },
+    expandArrays: function(a, b) {
+      var result = [];
+      a.forEach(function(ae) {
+        b.forEach(function(be) {
+          result.push(ae + be);
+        });
+      });
+      return result;
+    }
   };
 
-  $scope.words = [
-    {text: "WORD1"},
-    {text: "WORD2"},
-    {text: "WORD3"},
-    {text: "WORD4"}
-  ];
+  PERMS.calculate = function(numberString) {
+    console.log('calc');
+    if (numberString.length === 1) {
+      return PERMS.allCases[numberString];
+    }
+    var currentCases = PERMS.allCases[numberString[0]];
+    var remainingCases = PERMS.calculate(numberString.slice(1));
+    return PERMS.expandArrays(currentCases, remainingCases);
+  };
 
-  $scope.numberInput = '';
+  $scope.words = [];
 
   $scope.validateInput = function() {
     $scope.invalidInput = !$scope.numberInput.match(/^[\d -]+$/);
-  }
+  };
 
-
-
-  $scope.processNumber = function() {
-    var outputWord = [];
-    for (var i = 1; i < $scope.numberInput.length + 1; i++) {
-      for (var j = 0; j < $scope.letterSets[i].length; j++) {
-        console.log($scope.letterSets[i][j]);
-        outputWord[j] = $scope.letterSets[i][j];
-      }
-
-    }
-    console.log(outputWord);
-
-
-    $scope.words.push({ text: $scope.numberInput });
-    // console.log($scope.words);
+  $scope.processWords = function(numberString) {
+    $scope.words = PERMS.calculate(numberString);
   }
 
 }
-
-
-
 
 app.controller('N2WCtrl', N2WCtrl);
